95764bff71bf03b46ac0031dd653e55cbe9158f2
hawkf07 <fikrfifadilah299@gmail.com>
2023-08-17 19:18:55 +0700

    feat:todos functionalities

--- src/router/functions.ts
+++ src/router/functions.ts
@@ -1,7 +1,13 @@
 import { Router } from "express";
 import { db } from "../db";
 import { eq } from "drizzle-orm";
-import { notes, notesRelations, user, userRelations } from "../db/schema";
+import {
+  notes,
+  notesRelations,
+  todos,
+  user,
+  userRelations,
+} from "../db/schema";
 import { RequestWithUser } from "../types";
 
 const router = Router();
@@ -80,7 +86,7 @@ router.patch("/update_notes", async (req: RequestWithUser, res) => {
         date_updated,
       })
       .where(eq(notes.id, selectedNoteId));
-      res.send("Successfully updated note").status(200)
+    res.send("Successfully updated note").status(200);
   } catch (error) {
     res.send({ message: "error " + error });
   }
@@ -88,5 +94,90 @@ router.patch("/update_notes", async (req: RequestWithUser, res) => {
 /*
 todos functionalities (CRUD Operation)
 */
+// get notes
+router.get("/get_todos", async (req: RequestWithUser, res) => {
+  try {
+    const userData = await db.query.user.findMany({
+      with: {
+        todos: true,
+      },
+      where: eq(req.user.id, user.id),
+    });
+    res
+      .status(200)
+      .send({ message: "successfuly get todo", todos: userData[0].todos });
+  } catch (error) {
+    console.log(req.user, req.cookies);
+    res.send({ message: "Error" + error }).status(400);
+  }
+});
+// add new notes
+router.post("/add_todos", async (req: RequestWithUser, res) => {
+  try {
+    const { title, description } = req.body;
+    const authorId = req.user.id;
+    console.log(authorId);
+    const newNote = await db
+      .insert(todos)
+      .values({
+        authorId,
+        description,
+        title,
+      })
+      .returning();
+    return res
+      .send({
+        message: "created new todos",
+        id: newNote[0].id,
+      })
+      .status(200);
+  } catch (error) {
+    res.send({ message: "error" + error }).status(400);
+  }
+});
+// delete existing note
+router.delete("/delete_todos", async (req: RequestWithUser, res) => {
+  try {
+    const { rowId } = req.body;
+    const deletedTodo = await db
+      .delete(notes)
+      .where(eq(rowId, notes.id))
+      .returning();
 
+    return res
+      .send({
+        message: "deleted todos with rowId + " + rowId,
+        id: deletedTodo[0].id,
+      })
+      .status(200);
+  } catch (error) {
+    res.send({
+      message: "error " + error,
+    });
+  }
+});
+// update note
+router.patch("/update_todos/:id", async (req: RequestWithUser, res) => {
+  try {
+    const { selectedTodoId, title, description, body } = req.body;
+    const date_updated = new Date().toDateString();
+
+    const updatedTodo = await db
+      .update(todos)
+      .set({
+        description,
+        title,
+        date_updated,
+      })
+      .where(eq(todos.id, selectedTodoId))
+      .returning();
+    res
+      .send({
+        message: "todos with id:" + updatedTodo[0].id,
+      })
+      .status(200);
+  } catch (error) {
+    res.send({ message: "error " + error });
+  }
+});
 export default router;
